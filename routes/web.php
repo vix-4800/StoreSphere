91acd1d70ed2b402c5ba0d54780bdd4e960893e9
vix-4800 <Gordan.ei@protonmail.ch>
2024-02-07 16:24:30 +0400

    Init commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Routing\RedirectController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [RedirectController::class, 'index']);
+Route::get('/cart', [RedirectController::class, 'cart']);
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
