ff672470a6325b0bf7c1f9bf3458ebfce5baa9c9
vix-4800 <Gordan.ei@protonmail.ch>
2024-02-13 10:39:24 +0400

    ItemCard component removed from index page

--- app/Livewire/Index/ItemsTable.php
+++ app/Livewire/Index/ItemsTable.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire\Index;
 
+use App\Models\CartItem;
 use App\Models\Item;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class ItemsTable extends Component
@@ -11,18 +13,50 @@ class ItemsTable extends Component
 
     public function mount()
     {
-        $this->userItems = auth()->check() ? auth()->user()->cartItems->pluck('item_id') : collect();
+    protected $perPage = 12;
     }
 
+    #[On('item-quantity-change')]
     public function render()
     {
+        $this->userItems = auth()->check() ?
+            auth()->user()->cartItems->pluck('item_id') :
+            collect();
+
         return view('livewire.index.items-table', [
-            'items' => Item::paginate(12)
+            'items' => Item::paginate($this->perPage),
         ]);
     }
 
-    public function updatedPage($page)
+    public function add(int $itemId)
     {
-        $this->render();
+        $item = CartItem::firstWhere('item_id', $itemId);
+
+        if ($item) {
+            $item->quantity++;
+            $item->save();
+        } else {
+            CartItem::create([
+                'user_id' => auth()->id(),
+                'item_id' => $itemId,
+                'quantity' => 1
+            ]);
+        }
+
+        $this->dispatch('item-quantity-change');
+    }
+
+    public function remove(int $itemId)
+    {
+        $item = CartItem::firstWhere('item_id', $itemId);
+
+        $item->quantity--;
+        if ($item->quantity == 0) {
+            $item->delete();
+        } else {
+            $item->save();
+        }
+
+        $this->dispatch('item-quantity-change');
     }
 }
